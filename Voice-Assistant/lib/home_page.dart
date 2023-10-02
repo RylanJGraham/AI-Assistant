74104cb2b7bc91e6ce82c6873e9c854904b3917f
Rylan Graham <95762889+RylanJGraham@users.noreply.github.com>
2023-10-02 12:18:51 +0200

    updated

--- Voice-Assistant/lib/home_page.dart
+++ Voice-Assistant/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:allen/feature_box.dart';
 import 'package:allen/pellete.dart';
 import 'package:flutter/material.dart';
 
@@ -49,12 +50,53 @@ class _HomePageState extends State<HomePage> {
               top: 30,
             ),
             decoration:
-                BoxDecoration(border: Border.all(color: Pallete.borderColor)),
-            child: const Text('Good Morning, what can I do for you?',
-                style: TextStyle(
-                  color: Pallete.mainFontColor,
-                )),
-          )
-        ]));
+                BoxDecoration(border: Border.all(color: Pallete.borderColor),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                ),
+              ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text('Good Morning, what can I do for you?',
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
+                  )),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only( top:10,left:22,),
+            child: const Text(
+              'Here are a few features to try', 
+              style: TextStyle(
+              fontFamily: 'Cera Pro',
+              color: Pallete.mainFontColor,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        // Features List
+        Column(
+          children: const [
+            FeatureBox(color: Pallete.firstSuggestionBoxColor,
+            headerText: 'ChatGPT', descriptionText: 'A smarter way to stay organized and informed with ChatGPT',
+            ),
+            FeatureBox(color: Pallete.secondSuggestionBoxColor,
+            headerText: 'Dall-E', 
+            descriptionText: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
+            ),
+            FeatureBox(color: Pallete.thirdSuggestionBoxColor,
+            headerText: 'Smart Voice Assistant', 
+            descriptionText: 'Get the best of both worlds with voice assistant powered by Dall-E and ChatGPT',
+            ),
+          ],
+        ),
+        ],
+      ),
+    );
   }
 }
